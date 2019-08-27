5abd4c89dbbb6f30f9c272b4c3b8c3b9a1f6ad8c
Bruno Romeiro <bruno.romeiro@gmail.com>
2019-08-27 15:39:27 -0300

    feat(test): exemplo de uso

---
+++ tests/main.spec.js
@@ -0,0 +1,19 @@
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen blabla', () => {
+        // Espera o que aconteça
+        // Entra de dados / método sum (2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+
+    context('Case 2', () => {
+
+    });
+  });
+
+  describe('Method B', () => {
+
+  });
+});
